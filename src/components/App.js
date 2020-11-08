4d2f5c13f9f6bb69fdece15ed1a05b614ca0d296
Shaik-Mohammed-Zubaidi <68868510+Shaik-Mohammed-Zubaidi@users.noreply.github.com>
2020-11-08 16:29:49 +0530

    ndkcdv

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,49 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
+  constructor() {
+    super();
+    this.tid = 0;
+    this.state = {
+      timehr: 0,
+      timemin: 0,
+      timesec: 0
+    };
+  }
+  componentDidMount() {
+    var date = new Date();
+    this.setState({
+      timehr: date.getHours(),
+      timemin: date.getMinutes(),
+      timesec: date.getSeconds()
+    });
+    this.tid = setInterval(() => {
+      var date = new Date();
+      this.setState({
+        timehr: date.getHours(),
+        timemin: date.getMinutes(),
+        timesec: date.getSeconds()
+      });
+    }, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.tid);
+  }
 
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <div className="Clock">
+        <h3 id="time">
+          `{this.state.timehr}:{this.state.timemin}:
+          {this.state.timesec < 10
+            ? "0" + this.state.timesec
+            : this.state.timesec}{" "}
+          {this.state.timehr > 12 ? "PM" : "AM"}`
+        </h3>
+      </div>
+    );
+  }
 }
-
 
 export default App;
